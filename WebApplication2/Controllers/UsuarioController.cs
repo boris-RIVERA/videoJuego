7b93bf73d58e1046246e19dd43b4bf1559cb950c
boris-RIVERA <147168425+boris-RIVERA@users.noreply.github.com>
2023-10-23 23:50:56 -0400

    agregadoUsuarioController

--- WebApplication2/Controllers/UsuarioController.cs
+++ WebApplication2/Controllers/UsuarioController.cs
@@ -1,72 +1,69 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Context;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class UsuarioController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
+   
+        [ApiController]
+        [Route("[controller]")]
+        public class UsuarioController : ControllerBase
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+           
+            private readonly ILogger<UsuarioController> _logger;
+            private readonly AplicacionContexto _aplicacionContexto;
+            public UsuarioController(
+                ILogger<UsuarioController> logger,
+                AplicacionContexto aplicacionContexto)
+            {
+                _logger = logger;
+                _aplicacionContexto = aplicacionContexto;
+            }
+            //[HttpPost(Name = "CrearEstudiante")]
+            [HttpPost]
+            [Route("usuario")]
+            public async Task<IActionResult> Post([FromBody] Usuario usuario)
+            {
+                _aplicacionContexto.Usuario.Add(usuario);
+                _aplicacionContexto.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK, "creado");
+            }
 
-        private readonly ILogger<UsuarioController> _logger;
-        private readonly AplicacionContexto _aplicacionContexto;
-        public UsuarioController(
-            ILogger<UsuarioController> logger,
-            AplicacionContexto aplicacionContexto)
-        {
-            _logger = logger;
-            _aplicacionContexto = aplicacionContexto;
+
+            // [HttpGet(Name = "GetEstudiante")]
+
+            [HttpGet]
+            [Route("MostrarEstudiante")]
+            public async Task<IActionResult> Get()
+            {
+                List<Usuario> listUsuario = _aplicacionContexto.Usuario.ToList();
+
+                return StatusCode(StatusCodes.Status200OK, listUsuario);
+            }
+
+            //[HttpPut(Name = "PutEstudiante")]
+            [HttpPut]
+            [Route("EditarEstudiante/")]
+            public async Task<IActionResult> Edit([FromBody] Usuario Usuario)
+            {
+                _aplicacionContexto.Usuario.Update(Usuario);
+                _aplicacionContexto.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK, "editado");
+
+
+            }
+
+            [HttpDelete]
+            [Route("EliminarEstudiante/")]
+            //[HttpDelete(Name = "DeleteEstudiante")]
+            public async Task<IActionResult> Delete(int? id)
+            {
+                Usuario usuario = _aplicacionContexto.Usuario.Find(id);
+                _aplicacionContexto.Usuario.Remove( usuario);
+                _aplicacionContexto.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, "eliminado");
+            }
         }
-        //[HttpPost(Name = "CrearUsuario")]
-        [HttpPost]
-        [Route("usuario")]
-        public async Task<IActionResult> Post([FromBody] Usuario usuario)
-        {
-            _aplicacionContexto.usuario.AllAsync(usuario);
-            _aplicacionContexto.SaveChangesAsync();
-            return StatusCode(StatusCodes.Status200OK, "creado");
-        }
-
-
-        // [HttpGet(Name = "GetEstudiante")]
-
-        [HttpGet]
-        [Route("MostrarEstudiante")]
-        public async Task<IActionResult> Get()
-        {
-            List<VideoJuego> listUsuario = _aplicacionContexto.usuario.OrderByDescending(Usuario => Usuario.ReferenceEquals)
-                .ToList();
-
-            return StatusCode(StatusCodes.Status200OK, listUsuario);
-        }
-
-        //[HttpPut(Name = "PutEstudiante")]
-        [HttpPut]
-        [Route("EditarEstudiante/")]
-        public async Task<IActionResult> Edit([FromBody] Usuario usuario)
-        {
-            _aplicacionContexto.usuario.Update(usuario);
-            _aplicacionContexto.SaveChangesAsync();
-            return StatusCode(StatusCodes.Status200OK, "editado");
-
-
-        }
-
-        [HttpDelete]
-        [Route("EliminarEstudiante/")]
-        //[HttpDelete(Name = "DeleteEstudiante")]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            Usuario usuario = _aplicacionContexto.usuario.Find(id);
-            _aplicacionContexto.usuario.Remove(usuario);
-            _aplicacionContexto.SaveChanges();
-            return StatusCode(StatusCodes.Status200OK, "eliminado");
-        }
-    }
+    
 }
